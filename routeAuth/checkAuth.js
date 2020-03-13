a34c5c89eae5b3d93f1baa4d90206fc10a9814a6
Naeem <na6574113@gmail.com>
2020-03-13 16:50:00 +0500

    add route authntication

--- routeAuth/checkAuth.js
+++ routeAuth/checkAuth.js
@@ -1,8 +1,10 @@
 const jwt = require ('jsonwebtoken');
+const key = require ('../configure/key');
+
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split (' ')[1];
-    const decode = jwt.verify (token, process.env.JWTKEY);
+    const decode = jwt.verify (token, key.JWTKEY);
     res.userData = decode;
     next ();
   } catch (error) {
