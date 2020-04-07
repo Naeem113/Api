f3fce18e7a95aee90e656c6f2f30c5809cab5743
Naeem Akram <na6574113@gmail.com>
2020-04-07 15:23:32 +0500

    Hotel API

--- configure/dev.js
+++ configure/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoKey: 'mongodb+srv://naeem:naeem113@cluster0-f4q4i.mongodb.net/test?retryWrites=true&w=majority',
+  mongoKey: 'mongodb+srv://naeem:naeem113@hotelcluster-w9wwf.mongodb.net/HotelDataBase?retryWrites=true&w=majority',
   JWTKEY: 'secret',
 };
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require ('express');
-const productRoute = require ('./routes/products');
-const orderRoute = require ('./routes/order');
-const signupRoute = require ('./routes/signup');
-const signinRoute = require ('./routes/signin');
+const CityRoute = require ('./routes/city');
+const HotelRoute = require ('./routes/Hotel');
+const CommentsRoute = require ('./routes/comments');
+
 const key = require ('./configure/key');
 const morgan = require ('morgan');
 const bodyParser = require ('body-parser');
@@ -31,10 +31,9 @@ app.use ((req, res, next) => {
   next ();
 });
 
-app.use ('/product', productRoute);
-app.use ('/order', orderRoute);
-app.use ('/signup', signupRoute);
-app.use ('/signin', signinRoute);
+app.use ('/cities', CityRoute);
+app.use ('/comments', CommentsRoute);
+app.use ('/hotels', HotelRoute);
 
 app.use ((req, res, next) => {
   const error = new Error ('Not found');
